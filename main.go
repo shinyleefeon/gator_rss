dd75574bc376eecb016a958531540500b4633b29
shinyleefeon <leecastro7@gmail.com>
2025-12-17 10:20:55 -0500

    Setup up a database of users that is handled with the current user handling cli commands

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
  "fmt"
  "errors"
  "os"
+ "context"
+ "time"
+ "database/sql"
+ "github.com/google/uuid"
+ "github.com/shinyleefeon/gator_rss/internal/database"
 )
+
+import _ "github.com/lib/pq"
 
 type state struct {
 	config *config.Config
+	db *database.Queries
 
 }
 
@@ -25,11 +33,52 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) < 1 { 
 		return errors.New("Username is required")
 	}
-	err := s.config.SetUser(cmd.args[0])
+
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("User %s does not exist", cmd.args[0])
+		os.Exit(1)
+	}
+
+
+	err = s.config.SetUser(cmd.args[0])
 	if err != nil {
 		return err
 	}
 	fmt.Println("Username set to:", s.config.Current_user_name)
+	return nil
+}
+
+
+
+
+func registerUser(s *state, cmd command) error {
+	if len(cmd.args) < 1 { 
+		return errors.New("Username is required")
+	}
+	username := cmd.args[0]
+
+	_, err := s.db.GetUser(context.Background(), username)
+	if err == nil {
+		return fmt.Errorf("User %s already exists", username)
+	}
+
+	userParams := database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name: username,
+	}
+
+	user, err := s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		return err
+	}
+
+	s.config.SetUser(username)
+	fmt.Println("User registered and set as current user:", username)
+	fmt.Printf("User details: %+v\n", user)
+
 	return nil
 }
 
@@ -53,8 +102,18 @@ func main() {
 
 	s := &state{config: cfg}
 
+	// Initialize database connection, takes database package and initializes it to state struct
+	db, err := sql.Open("postgres", cfg.Db_url)
+	if err != nil {
+		panic(err)
+	}
+	s.db = database.New(db)
+	
+	
+	
 	InitCommands := commands{command_map: make(map[string]func(*state, command) error)}
 	InitCommands.register("login", handlerLogin)
+	InitCommands.register("register", registerUser)
 
 	input := os.Args
 	if len(input) < 2 {
